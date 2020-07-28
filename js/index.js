9d483355c384a1e691db840ce9893df0d0a66281
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-07-28 17:54:12 +0600

    feat: added some js code

--- js/index.js
+++ js/index.js
@@ -14,9 +14,26 @@ import {
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
 
-playerBtn.forEach(function (item) {
-  item.addEventListener('click', function () {
-    console.log('a');
+const deactivationPlayer = () => {
+  temp.style.display = 'none';
+  playerBtn.forEach((item) => {
+    item.classList.remove('active');
+  });
+  playerBlock.forEach((item) => {
+    item.classList.remove('active');
+  });
+};
+
+playerBtn.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    deactivationPlayer();
+    item.classList.add('active');
+    playerBlock[index].classList.add('active');
   });
 });
+
+radioPlayerInit();
+audioPlayerInit();
+videoPlayerInit();
