57de27f9b77dbea55801bf50b95a28e893def88e
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-07-29 20:28:01 +0600

    feat: added radio functional

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,3 +1,55 @@
 export const radioPlayerInit = () => {
-  console.log('radio init');
-}
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioHeaderBig = document.querySelector('.radio-header__big');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+
+  const audio = new Audio();
+  audio.type = 'audio/aac';
+
+  radioStop.disabled = true;
+
+  const changeIconPlay = () => {
+    if (audio.paused) {
+      radio.classList.remove('play');
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    } else {
+      radio.classList.add('play');
+      radioStop.classList.add('fa-stop');
+      radioStop.classList.remove('fa-play');
+    }
+  };
+
+  const selectItem = (elem) => {
+    radioItem.forEach((item) => item.classList.remove('select'));
+    elem.classList.add('select');
+  };
+
+  radioNavigation.addEventListener('change', (event) => {
+    let target = event.target;
+    const dataStantion = target.dataset.radioStantion;
+    const parent = target.closest('.radio-item');
+    const title = parent.querySelector('.radio-name').textContent;
+    const img = parent.querySelector('.radio-img').src;
+    radioHeaderBig.textContent = title;
+    radioCoverImg.src = img;
+
+    selectItem(parent);
+    radioStop.disabled = false;
+    audio.src = dataStantion;
+    audio.play();
+    changeIconPlay();
+  });
+
+  radioStop.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    changeIconPlay();
+  });
+};
